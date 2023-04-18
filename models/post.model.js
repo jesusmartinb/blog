e3df930321b9ce64a54a65c3f79039e9dfe8ea18
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-04-18 16:36:15 +0200

    Se modifica la query que obtiene la totalidad de posts para que cada posts muestre la información de su autor

--- models/post.model.js
+++ models/post.model.js
@@ -1,17 +1,20 @@
 // Obtención de los posts
 // SELECT * FROM posts
+// Cada post recuperado va acompañado de todos los datos del autor de este y no solo del identificador que lo define
+// select p.titulo, p.descripcion, p.fecha_creacion as 'fecha creación', p.categoria, a.nombre as 'autor', a.email, a.imagen from blog_unir.posts as p, blog_unir.autores as a where p.fk_autores = a.id;
 const getAll = () => {
-	return db.query('select * from posts');
+	// return db.query('select * from posts');
+	return db.query("select p.titulo, p.descripcion, p.fecha_creacion as 'fecha creación', p.categoria, a.nombre as 'autor', a.email, a.imagen from blog_unir.posts as p, blog_unir.autores as a where p.fk_autores = a.id");
 }
 
 // Creación de un nuevo registro de post
-// insert into posts (descripcion, fecha_creacion, categoria, fk_autores) values (?, ?, ?, ?)
-const create = ({ descripcion, fecha_creacion = new Date(), categoria, fk_autores }) => {
+// insert into posts (titulo, descripcion, fecha_creacion, categoria, fk_autores) values (?, ?, ?, ?, ?)
+const create = ({ titulo, descripcion, fecha_creacion = new Date(), categoria, fk_autores }) => {
 	fecha_creacion = fecha_creacion.getFullYear() + "-" + (fecha_creacion.getMonth() + 1) + "-" + fecha_creacion.getDate();
 
 	return db.query(
-		'insert into posts (descripcion, fecha_creacion, categoria, fk_autores) values (?, ?, ?, ?)',
-		[descripcion, fecha_creacion, categoria, fk_autores]
+		'insert into posts (titulo, descripcion, fecha_creacion, categoria, fk_autores) values (?, ?, ?, ?, ?)',
+		[titulo, descripcion, fecha_creacion, categoria, fk_autores]
 	)
 }
 
